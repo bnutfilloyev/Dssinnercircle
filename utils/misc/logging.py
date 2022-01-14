6f9a4230058c0ea50b72eead12ef715a526a36e7
Bexruz Nutfilloyev <63140958+YoshlikMedia@users.noreply.github.com>
2022-01-15 03:07:47 +0500

    DEBUG -> INFO

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -1,5 +1,5 @@
 import logging
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.DEBUG,
+                    level=logging.INFO,
                     )
