4bc699d4c2c0bba3b8d7012662abe79afbb061fa
Bexruz Nutfilloyev <63140958+YoshlikMedia@users.noreply.github.com>
2022-01-16 11:53:26 +0500

    fix info button

--- handlers/users/infoButton.py
+++ handlers/users/infoButton.py
@@ -1,10 +1,10 @@
 from aiogram import types
 
-from data.text import text
+from data.text import text, default_button
 from filters import IsPrivate
 from loader import dp
 
-@dp.message_handler(IsPrivate(), text='ℹ️Info')
+@dp.message_handler(IsPrivate(), text=default_button['info'])
 async def get_plans(m: types.Message):
     await m.answer(text=text['info'])
 
