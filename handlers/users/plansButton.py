7ccb949f96cfa1a5f47f1d94444d8ba8c7be8fad
Bexruz Nutfilloyev <63140958+YoshlikMedia@users.noreply.github.com>
2022-01-16 11:39:21 +0500

    change texts

--- handlers/users/plansButton.py
+++ handlers/users/plansButton.py
@@ -5,8 +5,8 @@ from filters import IsPrivate
 from keyboards.inline.plan_keyboards import plansMenu
 from loader import dp
 
-@dp.message_handler(IsPrivate(), commands='planos')
-@dp.message_handler(IsPrivate(), text='Plans')
+# @dp.message_handler(IsPrivate(), commands='planos')
+@dp.message_handler(IsPrivate(), text='Planos')
 async def get_plans(m: types.Message):
     await m.answer(text=text['plan'], reply_markup=plansMenu)
 
