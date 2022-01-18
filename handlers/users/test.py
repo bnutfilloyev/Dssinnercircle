ad469d1a0fdaad2e2d68e821a83d12511059780e
Bexruz Nutfilloyev <63140958+YoshlikMedia@users.noreply.github.com>
2022-01-18 18:16:29 +0500

    add new bot commands

--- handlers/users/test.py
+++ handlers/users/test.py
@@ -1,9 +1,59 @@
-from utils.db_api.mongo import plans_price_db
-
-plans = plans_price_db.find_one()
-print(plans)
-
-plans = plans_price_db.find_one_and_update({}, update={
-    '$set': {'plans_price': ['10', '64', '90', '610'], 'plans_days': [30, 90, 30, 365]}
-}, return_document=True)
-print(plans)
+# import stripe
+#
+# stripe.api_key = "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"
+# amount = 1000
+# currency = 'USD'
+# bot_name = 'test'
+#
+# price = stripe.Price.create(
+#     unit_amount=amount,
+#     currency=currency.lower(),
+#     product="prod_KyDBeasX5H30dU",
+# )
+#
+# payment = stripe.checkout.Session.create(
+#     success_url=f"http://t.me/{bot_name}",
+#     cancel_url=f"http://t.me/{bot_name}",
+#     line_items=[
+#         {
+#             "price": price.id,
+#             "quantity": 1,
+#         },
+#     ],
+#     mode="payment",
+# )
+#
+# print(payment.url)
+# #
+# # import paypalrestsdk
+# # import logging
+# #
+# # paypalrestsdk.configure({
+# #   "mode": "live", # sandbox or live
+# #   "client_id": "",
+# #   "client_secret": "" })
+# #
+# # payment = paypalrestsdk.Payment({
+# #     "intent": "sale",
+# #     "payer": {
+# #         "payment_method": "paypal"},
+# #     "redirect_urls": {
+# #         "return_url": "http://localhost:3000/payment/execute",
+# #         "cancel_url": "http://localhost:3000/"},
+# #     "transactions": [{
+# #         "item_list": {
+# #             "items": [{
+# #                 "name": "item",
+# #                 "sku": "item",
+# #                 "price": "5.00",
+# #                 "currency": "USD",
+# #                 "quantity": 1}]},
+# #         "amount": {
+# #             "total": "5.00",
+# #             "currency": "USD"},
+# #         "description": "This is the payment transaction description."}]})
+# #
+# # if payment.create():
+# #   print("Payment created successfully")
+# # else:
+# #   print(payment.error)
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,7 +4,12 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Run bot"),
-            types.BotCommand("help", "Help"),
+            types.BotCommand("start", "Inicializa o bot"),
+            types.BotCommand("planos",  "Esta opção lista todos os planos que pode escolher"),
+            types.BotCommand("status", "Esta opção dá informação do estado da sua subscrição"),
+            types.BotCommand("subscrever", "Esta opção lista todos os planos que pode escolher"),
+            types.BotCommand("cancelar", "Esta opção cancela a sua subscrição (não será mais cobrado no final do termo da mesma)"),
+            types.BotCommand("help", "Mostra este menu de ajuda"),
+            types.BotCommand("info", "Disponibiliza todas as informações sobre o grupo DSS Insider e os seus benefícios"),
         ]
     )
