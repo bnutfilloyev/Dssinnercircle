31795066fa8a840ebb912b067f999c7e216daf0a
Bexruz Nutfilloyev <63140958+YoshlikMedia@users.noreply.github.com>
2022-01-19 15:11:57 +0500

    auto kick modified

--- utils/check_status_user.py
+++ utils/check_status_user.py
@@ -2,6 +2,7 @@ import asyncio
 
 from data.config import CHANEL, ADMINS
 from data.text import text
+from handlers.groups.moderator import ban_member
 from loader import bot
 from utils import on_startup_notify
 from utils.db_api.mongo import user_db
@@ -13,11 +14,11 @@ async def check_status_user():
         if not i['status']:
             await bot.send_message(i['telegram_id'], text=text['kick_notification_user'])
             await asyncio.sleep(100)
-
             for admin in ADMINS:
                 await bot.send_message(admin, text=text['kick_notification_admin'])
-
             await bot.send_message(CHANEL, text=text['kick_notification_admin'])
+            await ban_member()
+
         else:
             user_db.update_one({'telegram_id': i['telegram_id']}, {
                 "$set": {
