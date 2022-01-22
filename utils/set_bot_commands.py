91db97456eddcdd3bcc64035ab226ae07adeb71a
yoshlikmedia <yoshlikmedia@gmail.com>
2022-01-22 15:13:40 +0500

    bug fix

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -7,7 +7,8 @@ async def set_default_commands(dp):
             types.BotCommand("start", "Inicializa o bot"),
             types.BotCommand("planos",  "Esta opção lista todos os planos que pode escolher"),
             types.BotCommand("status", "Esta opção dá informação do estado da sua subscrição"),
-            types.BotCommand("subscrever", "Esta opção lista todos os planos que pode escolher"),
+            # types.BotCommand("subscrever", "Esta opção lista todos os planos que pode escolher"),
+            types.BotCommand("cancelar", "Esta opção cancela a sua subscrição (não será mais cobrado no final do termo da mesma)"),
             types.BotCommand("help", "Mostra este menu de ajuda"),
             types.BotCommand("info", "Disponibiliza todas as informações sobre o grupo DSS Insider e os seus benefícios"),
         ]
